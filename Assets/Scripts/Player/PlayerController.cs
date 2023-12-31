307cecedb654525dd25113533bd77febe096fc8f
Lina <41321441+Linamcaro@users.noreply.github.com>
2023-12-31 15:09:31 -0500

    orb collection system, fixed player jump and orb collection text

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,119 +6,150 @@ public class PlayerController : MonoBehaviour
 {
 
     [Header("MOVEMENT")]
-    [SerializeField] private float grounderDistance = 0.05f;
-    [SerializeField] private float maxSpeed = 14;
-    [SerializeField] private float acceleration = 120;
-    [SerializeField] private float groundDeceleration = 40;
-    [SerializeField] private float airDeceleration = 30;
-    [SerializeField] private float jumpPower = 36;
-    [SerializeField] private float maxFallSpeed = 40;
-    [SerializeField] private float fallAcceleration = 110;
-    [SerializeField] private float groundingForce = -1.5f;
-    [SerializeField] private float jumpEndEarlyGravityModifier = 3;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private float ceilingCheckRadius;
+    [SerializeField] private float groundDeceleration;
+    [SerializeField] private float airDeceleration;
+    [SerializeField] private float groundingForce;
+    [SerializeField] private float fallAcceleration;
+    [SerializeField] private float maxfallSpeed;
 
-    private FrameInput frameInput;
-    private Rigidbody2D playerRb;
+    [Header("COLLISIONS")]
+    // reference to the ground layer objects
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform ceilingCheck;
+
+    private bool isFacingRight = true;
     private Vector2 playerVelocity;
-    private CapsuleCollider2D _col;
-    [SerializeField] private bool isGrounded;
+    private Rigidbody2D playerRb;
+    private FrameInput frameInput;
 
     // Start is called before the first frame update
     void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
-        _col = GetComponent<CapsuleCollider2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
         GetInput();
+        Movement();
+        Jump();
     }
+
     private void FixedUpdate()
     {
-
-        CheckCollisions();
-        Jump();
-        HandleDirection();
         HandleGravity();
-        ApplyMovement();
-
     }
 
+    /// <summary>
+    /// Gather players input
+    /// </summary>
     private void GetInput()
     {
         frameInput = new FrameInput
         {
+            move = PlayerControls.Instance.GetPlayerMovement(),
             jump = PlayerControls.Instance.PlayerJumped(),
-            fire = PlayerControls.Instance.PlayerFired(),
-            move = PlayerControls.Instance.GetPlayerMovement()
-
+            fire = PlayerControls.Instance.PlayerFired()
         };
 
     }
 
-    private void CheckCollisions()
-    {
-        // Ground and Ceiling
-        bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, grounderDistance);
-        bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, grounderDistance);
-
-        // Hit a Ceiling
-        if (ceilingHit) playerVelocity.y = Mathf.Min(0, playerVelocity.y);
-
-        // Landed on the Ground
-        if(!isGrounded && groundHit)
-        {
-            isGrounded = true;
-        }
-        else if (isGrounded && !groundHit)
-        {
-            isGrounded = false;
-        }
-
-    }
-
-
+    /// <summary>
+    /// Perform jump
+    /// </summary>
     private void Jump()
     {
-        if (isGrounded && frameInput.jump)
+        if (frameInput.jump && IsGrounded())
         {
             playerVelocity.y = jumpPower;
         }
 
-     }
-
-    private void HandleDirection()
-    {
-        if (frameInput.move.x == 0)
+        if(!IsGrounded() && HitCeiling()) 
         {
-            var deceleration = isGrounded ? groundDeceleration : airDeceleration;
-            playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, 0, deceleration * Time.fixedDeltaTime);
-        }
-        else
-        {
-            playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, frameInput.move.x * maxSpeed, acceleration * Time.fixedDeltaTime);
+            playerVelocity.y = Mathf.Min(0, playerVelocity.y);
         }
     }
 
+ 
     private void HandleGravity()
     {
-        if (isGrounded && playerVelocity.y <= 0f)
+
+        if (IsGrounded() && playerVelocity.y <= 0f)
         {
             playerVelocity.y = groundingForce;
         }
         else
         {
             var inAirGravity = fallAcceleration;
-            if (playerVelocity.y > 0) inAirGravity *= jumpEndEarlyGravityModifier;
-            playerVelocity.y = Mathf.MoveTowards(playerVelocity.y, -maxFallSpeed, inAirGravity * Time.fixedDeltaTime);
+            playerVelocity.y = Mathf.MoveTowards(playerVelocity.y, -maxfallSpeed, inAirGravity * Time.deltaTime);
         }
     }
 
-    private void ApplyMovement() => playerRb.velocity = playerVelocity;
+    /// <summary>
+    /// Handle Player Movement
+    /// </summary>
+    private void Movement()
+    {
+        if (frameInput.move.x == 0)
+        {
+            var deceleration = IsGrounded() ? groundDeceleration : airDeceleration;
+            playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, 0, deceleration * Time.deltaTime);
+        }
+        else if (IsGrounded() && !HitCeiling())
+        {
+            playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, frameInput.move.x * maxSpeed, acceleration * Time.fixedDeltaTime);
+        }
 
+
+        if (!isFacingRight && frameInput.move.x > 0)
+        {
+            Flip();
+        }
+        else if (isFacingRight && frameInput.move.x < 0)
+        {
+            Flip();
+        }
+
+        playerRb.velocity = playerVelocity;
+    }
+
+    /// <summary>
+    /// Check if player is on the ground
+    /// </summary>
+    /// <returns></returns>
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);  
+
+    }
+    
+    //---- 
+
+    /// <summary>
+    /// Check if the player hit a ceiling
+    /// </summary>
+    /// <returns></returns>
+    private bool HitCeiling()
+    {
+        return Physics2D.OverlapCircle(ceilingCheck.position, ceilingCheckRadius, groundLayer);
+    }
+    
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+  
 }
 
 public struct FrameInput
